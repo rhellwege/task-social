25ce307cbb498066861f89734c8173b70e9a8fd7
Ryan Hellwege <rhellwege6@gmail.com>
2025-09-03 17:22:42 -0700

    [CI/CD] [Backend] Build workflow for backend + scripts

---
+++ backend/scripts/get_tools.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+set -x
+go install github.com/swaggo/swag/cmd/swag@latest
+go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
+go install github.com/air-verse/air@latest
+go install go.uber.org/nilaway/cmd/nilaway@latest
