19012abc5aeacd1e361f416beb9f2d6810dc5c2e
Ryan Hellwege <rhellwege6@gmail.com>
2025-09-04 22:20:44 -0700

    [CI/CD] [Backend] Build workflow for backend + scripts

---
+++ backend/scripts/check_codegen.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+cd internal/db
+pwd
+echo "Checking sqlc..."
+if [ -n "$(sqlc diff)" ]
+then
+    echo "Sqlc needs to be generated before pushing changes."
+    echo "run sqlc generate in internal/db/sql or use the Makefile."
+    exit 1
+fi
+echo "Sqlc is up to date."
+
+echo "Checking swagger..."
+cd ../../
+pwd
+cp -r docs docs.bak
+swag init -g cmd/main.go
+# Check if new docs differs from docs.bak
+if [ -n "$(diff -r docs docs.bak)" ]
+then
+    echo "Docs need to be generated before pushing changes."
+    echo "run swag init -g cmd/main.go in backend/ or use the Makefile."
+    rm -rf docs.bak
+    exit 1
+fi
+echo "Docs are up to date."
+rm -rf docs.bak
