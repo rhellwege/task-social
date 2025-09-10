e0bf60dbbcea600626809065b60e1dfff9f8c2c2
Keith Bui <kqbui@csu.fullerton.edu>
2025-09-09 19:31:54 -0700

    [Refactor] Changed layout to use a tabs and stacks based design
    Used https://github.com/astrojarred/tabs-v-stacks as a template

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -2,26 +2,45 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import { Tabs } from 'expo-router';
+import { Platform } from 'react-native';
+import { Colors } from '@/constants/Colors';
+import { HapticTab } from '@/components/HapticTab';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs 
+        screenOptions={{
+            tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+            headerShown: false,
+            tabBarButton: HapticTab,
+            tabBarStyle: Platform.select({
+              ios: {
+                position: 'absolute',
+              },
+              default: {},
+            }),
+          }}>
+
       <Tabs.Screen
-        name="clubs"
+        name="(clubs)"
         options={{
           title: 'Clubs',
           tabBarIcon: ({ color }) => <MaterialIcons name="people" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="index"
+        name="(index)"
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <SimpleLineIcons name="globe-alt" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="profile"
+        name="(profile)"
         options={{
           title: 'Me',
           tabBarIcon: ({ color }) => <Ionicons name="person-circle-outline" size={24} color={color} />,
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,17 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function Layout() {
+  const colorScheme = useColorScheme();
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
