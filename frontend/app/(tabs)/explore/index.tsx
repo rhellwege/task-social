b1937a3a8dbb905a1991c7a62a53d8ad7bbf93dd
Ryan Hellwege <rhellwege6@gmail.com>
2025-12-15 16:14:46 -0800

    fake clubs

--- frontend/app/(tabs)/explore/index.tsx
+++ frontend/app/(tabs)/explore/index.tsx
@@ -29,7 +29,7 @@ export default function Tab() {
           const response = await api.api.getPublicClubs();
           setClubs(response.data || []);
         } catch (error) {
-          console.error("Failed to fetch public clubs:", error);
+          console.log("Failed to fetch public clubs:", error);
         } finally {
           setIsLoading(false);
         }
